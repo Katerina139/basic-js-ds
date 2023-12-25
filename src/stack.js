249df79118dc7e79ab7e7d11c94926c59ed0df94
Katerina Ivankova <katerina.ivankova@mail.ru>
2023-12-25 21:32:50 +0300

    feat: add solution stack task

--- src/stack.js
+++ src/stack.js
@@ -12,20 +12,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * stack.pop(); // undefined
  *
  */
+const arr = [];
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  push(element) {
+    arr.push(element);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let j = arr[arr.length - 1];
+    arr.splice(arr.length -1, 1);
+    return j;
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return arr[arr.length - 1];
   }
 }
 
